1ee939c0b1a02b86dec84c4a9febb232b7a4749b
kadrikotkas <56673572+kadrikotkas@users.noreply.github.com>
2019-11-20 11:20:31 +0200

    Add files via upload

---
+++ Iseiseisev töö 2.py
@@ -0,0 +1,6 @@
+number = int(input("Sisesta üks number:"))
+odd = number % 2
+if odd > 0 or number==6 or number==8 or number==10 or number==12 or number==14 or number==16 or number==18 or number==20 :
+    print ("Ei sobi")
+else:
+    print ("Sobib")
